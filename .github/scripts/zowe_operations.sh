6ddb97b4d9e1df247c4e691b1990dea4c94709ee
Athar Ramzan <atharramzan.ofi@gmail.com>
2025-07-05 19:53:43 +0530

    update

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
+# Create z/OSMF profile using environment variables
+zowe profiles create zosmf zosmf-profile \
+  --host "$ZOSMF_HOST" \
+  --port "$ZOSMF_PORT" \
+  --username "$ZOWE_USERNAME" \
+  --password "$ZOWE_PASSWORD" \
+  --reject-unauthorized false
+
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
   echo "Creating directory..."
   zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
