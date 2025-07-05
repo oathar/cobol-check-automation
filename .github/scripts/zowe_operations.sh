d692e3bab52c1578289d8d26249ac0f68d28194e
Athar Ramzan <atharramzan.ofi@gmail.com>
2025-07-05 21:41:46 +0530

    update01

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
+
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
-# No need to create a profile; Zowe CLI v8+ uses environment variables
-
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+# Directory check and create
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" \
+    --host "$ZOSMF_HOST" \
+    --port "$ZOSMF_PORT" \
+    --user "$ZOWE_USERNAME" \
+    --password "$ZOWE_PASSWORD" \
+    --reject-unauthorized false &>/dev/null; then
   echo "Creating directory..."
-  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck" \
+    --host "$ZOSMF_HOST" \
+    --port "$ZOSMF_PORT" \
+    --user "$ZOWE_USERNAME" \
+    --password "$ZOWE_PASSWORD" \
+    --reject-unauthorized false
 fi
 
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar"
+# Upload files
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" \
+  --recursive --binary-files "cobol-check-0.2.9.jar" \
+  --host "$ZOSMF_HOST" \
+  --port "$ZOSMF_PORT" \
+  --user "$ZOWE_USERNAME" \
+  --password "$ZOWE_PASSWORD" \
+  --reject-unauthorized false
 
+# Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" \
+  --host "$ZOSMF_HOST" \
+  --port "$ZOSMF_PORT" \
+  --user "$ZOWE_USERNAME" \
+  --password "$ZOWE_PASSWORD" \
+  --reject-unauthorized false
