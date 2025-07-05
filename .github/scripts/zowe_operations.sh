1a2e3680aa84cc2409e43c0ef01a9d64ce65a077
Athar Ramzan <atharramzan.ofi@gmail.com>
2025-07-05 20:50:15 +0530

    updated files

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
-# Create z/OSMF profile using environment variables
-zowe profiles create zosmf zosmf-profile \
-  --host "$ZOSMF_HOST" \
-  --port "$ZOSMF_PORT" \
-  --username "$ZOWE_USERNAME" \
-  --password "$ZOWE_PASSWORD" \
-  --reject-unauthorized false
+# No need to create a profile; Zowe CLI v8+ uses environment variables
 
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
   echo "Creating directory..."
