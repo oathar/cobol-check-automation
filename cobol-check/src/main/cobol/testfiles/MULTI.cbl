95ecea2031c108471b6229a43332d1df07ce76e6
Athar Ramzan <atharramzan.ofi@gmail.com>
2025-07-30 23:02:59 +0530

    update

---
+++ cobol-check/src/main/cobol/testfiles/MULTI.cbl
@@ -0,0 +1,13 @@
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MUL.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 A PIC 9 VALUE 10.
+       01 B PIC 9 VALUE 2.
+       01 C PIC 9.
+
+       PROCEDURE DIVISION.
+           MULTIPLY A BY B GIVING C
+           DISPLAY "A * B = " C
+           GOBACK.
